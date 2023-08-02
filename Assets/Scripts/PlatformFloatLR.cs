7bda7488a29096e47dbecf8aca162c0d8eefb55f
OngTH0117 <108652684+OngTH0117@users.noreply.github.com>
2023-08-03 00:28:17 +0800

    Updates 3/8 #1
    Make Assets
    - platform
    - spike
    - script to control on platform

---
+++ Assets/Scripts/PlatformFloatLR.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatormFloatLR : MonoBehaviour
+{
+     public float floatDistance = 1.0f;
+    public float floatSpeed = 1.0f;
+
+    private Vector3 startPosition;
+    private float timer;
+    private bool isPlayerOnPlatform = false;
+    private Transform playerTransform;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime * floatSpeed;
+        float xPosition = startPosition.x + Mathf.Sin(timer) * floatDistance;
+        Vector3 newPosition = new Vector3(xPosition, transform.position.y, transform.position.z);
+
+       
+        transform.position = newPosition;
+
+      
+        if (isPlayerOnPlatform && playerTransform != null)
+        {
+            Vector3 playerRelativePosition = playerTransform.position - transform.position;
+            playerTransform.position = newPosition + playerRelativePosition;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isPlayerOnPlatform = true;
+            playerTransform = collision.transform;
+            
+            playerTransform.SetParent(transform);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+       
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isPlayerOnPlatform = false;
+          
+            playerTransform.SetParent(null);
+            playerTransform = null;
+        }
+    }
+}
