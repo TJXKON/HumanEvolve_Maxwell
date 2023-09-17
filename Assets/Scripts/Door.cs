f43e8808d0bd4046ff69b82955058d797babf9aa
OngTH0117 <108652684+OngTH0117@users.noreply.github.com>
2023-09-17 21:54:08 +0800

    Level 4 complete

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Door : MonoBehaviour
 {
-     public GameObject lockedIndicator; // An object indicating that the door is locked
-    public GameObject unlockedIndicator; // An object indicating that the door is unlocked
+     public GameObject lockedIndicator; 
+    public GameObject unlockedIndicator; 
 
     private bool isLocked = true;
 
@@ -14,7 +15,7 @@ public class Door : MonoBehaviour
         UpdateIndicators();
     }
 
-    public void TryOpenDoor()
+   public void TryOpenDoor()
 {
     Debug.Log("Trying to open door. Locked: " + isLocked + " HasKey: " + InventoryManager.instance.HasKey());
     if (isLocked && InventoryManager.instance.HasKey())
@@ -22,12 +23,19 @@ public class Door : MonoBehaviour
         isLocked = false;
         UpdateIndicators();
         Debug.Log("Door unlocked and opened.");
-        // Implement any logic to transition to the next stage or level here
+
+        // Load the next scene here
+        LoadNextScene();
     }
     else
     {
         Debug.Log("Door is locked.");
     }
+}
+private void LoadNextScene()
+{
+    // Load the scene named "Boss Room"
+    SceneManager.LoadScene("Boss Room");
 }
 
     private void UpdateIndicators()
@@ -35,4 +43,6 @@ public class Door : MonoBehaviour
         lockedIndicator.SetActive(isLocked);
         unlockedIndicator.SetActive(!isLocked);
     }
+
+
 }
